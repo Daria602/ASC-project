2924c06ab5bfc4e5c6b92eca068c82d03906d47f
Andrei Gatej <andreigtj01@gmail.com>
2020-11-13 19:21:44 +0200

    feat: compute entropy for the words in a text

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 # -*- coding: UTF-8 -*-
-# print(r"This is unicode: {}".format(d.keys()[0]))
+import math
+import re
 
-# citire din fisier
-# eliminare spatii/semne de punctuatii
-# making all the letters small
-# stocarea lungimii textului
-# frecventa fiecarui caracter
-# probabilitatea pt fiecare caracter
-# entropia
+signs = '[0-9\.\-":;!\?„,\'”″“]|[—]\s?|\[.*\]'
+
+inputFile = open("input.txt", "r")
+letterDictionary = {}
+
+content = inputFile.read().lower()
+content = re.sub(signs, '', content)
+content = re.sub('\s{2,}', ' ', content)
+content = re.sub('[\n]', ' ', content)
+contentLen = len(content)
+
+inputFile.close()
+
+grpLenBoundary = 21
+
+print("The maximum length of a group of letters is ", grpLenBoundary - 1)
+
+content = content.split(' ')
+contentLen = len(content)
+
+wordDic = {}
+wordSumLen = 0
+
+for word in content:
+  wordSumLen += len(word)
+  
+  if word not in wordDic:
+    wordDic[word] = 1
+  else:
+    wordDic[word] += 1
+
+totalWords = contentLen
+wordAvg = wordSumLen / totalWords
+wordDic.update((x, (y/totalWords) * (math.log2(y/totalWords))) for x, y in wordDic.items())
+entropy = -(sum(wordDic.values()))
+
+print("Entropy for all words", entropy / wordAvg)
